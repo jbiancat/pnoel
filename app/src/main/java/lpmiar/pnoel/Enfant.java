c3ef0208cf063c37877036e08afc92a330c73fd0
Jean Biancat <biancat.jean@gmail.com>
2018-12-10 16:26:47 +0100

    Preparation du projet

---
+++ app/src/main/java/lpmiar/pnoel/Enfant.java
@@ -0,0 +1,49 @@
+package lpmiar.pnoel;
+
+import java.util.Calendar;
+
+public class Enfant {
+    private int age;
+    private int anneeNaissance;
+    private String prenom;
+    private String sexe;
+
+    public Enfant(int aN, String p, String s){
+        int now = Calendar.getInstance().get(Calendar.YEAR);
+        this.anneeNaissance = aN;
+        this.age = now - this.anneeNaissance;
+        this.sexe=s;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAnneeNaissance() {
+        return anneeNaissance;
+    }
+
+    public void setAnneeNaissance(int anneeNaissance) {
+        this.anneeNaissance = anneeNaissance;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+}
--- app/src/main/java/lpmiar/pnoel/MainActivity.java
+++ app/src/main/java/lpmiar/pnoel/MainActivity.java
@@ -1,13 +1,50 @@
 package lpmiar.pnoel;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Enfant> enfants = new ArrayList<>();
+    private JSONObject jsonObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RequestQueue rq = Volley.newRequestQueue(this);
+        String url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&facet=prenom&facet=sexe&facet=annee_naissance";
+        StringRequest sr = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    jsonObject = new JSONObject(response);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
     }
 }
