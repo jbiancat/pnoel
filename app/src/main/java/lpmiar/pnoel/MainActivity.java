107b9ccf54e5eb0b0972fb7c6460713bfcf62a1c
Julien <julien.bourdoiseau@sogeti.com>
2018-12-13 13:38:54 +0100

    coucou juan

--- app/src/main/java/lpmiar/pnoel/MainActivity.java
+++ app/src/main/java/lpmiar/pnoel/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Spinner;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,19 +15,32 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<Enfant> enfants = new ArrayList<>();
     private JSONObject jsonObject;
-
+    private List<String> tab = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        tab.add("Age");
+        for (Integer i = 0; i <13;  i ++){
+            tab.add(i.toString());
+        }
         super.onCreate(savedInstanceState);
+        Spinner listeAge = findViewById(R.id.spinner);
+
+        ArrayAdapter adapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                tab
+        );        //Enfin on passe l'adapter au Spinner et c'est tout
+        listeAge.setAdapter(adapter);
+
         setContentView(R.layout.activity_main);
         RequestQueue rq = Volley.newRequestQueue(this);
         String url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&facet=prenom&facet=sexe&facet=annee_naissance";
