63ff5556d28df6b65f4682e77b8cd3325cfcfa6d
BOURDOISEAU <julien.bourdoiseau@sogeti.com>
2019-01-14 11:37:05 +0100

    début filtre

--- app/src/main/java/lpmiar/pnoel/MainActivity.java
+++ app/src/main/java/lpmiar/pnoel/MainActivity.java
@@ -4,9 +4,13 @@ import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import com.google.gson.JsonArray;
@@ -25,6 +29,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,23 +39,31 @@ public class MainActivity extends AppCompatActivity {
     private JSONObject jsonObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final MainActivity context = this;
 
         setContentView(R.layout.activity_main);
-
+        final RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        final Spinner ddn = findViewById(R.id.spinner);
         List<String> tab = new ArrayList<>();
-        tab.add("Age");
-        for (Integer i = 0; i <13;  i ++){
+        tab.add("ddn");
+        for (Integer i = 1990; i <2018;  i ++){
             tab.add(i.toString());
         }
         super.onCreate(savedInstanceState);
         Spinner listeAge = findViewById(R.id.spinner);
-
-        ListView liste = findViewById(R.id.liste);
+        final ListView liste = findViewById(R.id.liste);
         final EnfantArrayAdapter  dataEnfant = new EnfantArrayAdapter(this,R.layout.list_item_layout, R.id.text1, enfants);
         liste.setAdapter(dataEnfant);
         Button button = findViewById(R.id.filtrer);
+        button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton)findViewById(selectedId);
+                String date_de_naissance = ddn.getSelectedItem().toString();
 
+                filtre(rb.getText().toString(), date_de_naissance, liste);
+            }
+        });
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tab);
         listeAge.setAdapter(dataAdapter);
@@ -69,8 +82,39 @@ public class MainActivity extends AppCompatActivity {
                                 JsonObject fields = ite.next().getAsJsonObject().get("fields").getAsJsonObject();
                                 Enfant enfant = new Enfant(fields.get("annee_naissance").getAsInt(), fields.get("prenom").getAsString(), fields.get("sexe").getAsString());
                                 dataEnfant.add(enfant);
+                                Log.i("enfant:", enfant.toString());
+
                             }
                     }
                 });
+        }
+        public void filtre(String sexe, String annee_naissance, ListView liste){
+            String url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&facet=prenom&facet=sexe&facet=annee_naissance";
+
+            if (! sexe.equals("Tous")){
+                url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&facet=prenom&facet=sexe&facet=annee_naissance";
+            }
+            if (! annee_naissance.equals("ddn")){
+                url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&facet=prenom&facet=sexe&facet=annee_naissance";
+            }
+            final EnfantArrayAdapter  dataEnfant = new EnfantArrayAdapter(this,R.layout.list_item_layout, R.id.text1, enfants);
+            dataEnfant.clear();
+            Ion.with(this)
+                    .load(url)
+                    .asJsonObject()
+                    .setCallback(new FutureCallback<JsonObject>() {
+                        @Override
+                        public void onCompleted(Exception e, JsonObject result) {
+
+                            JsonArray jArray = result.get("records").getAsJsonArray();
+                            Iterator<JsonElement> ite = jArray.iterator();
+                            while (ite.hasNext()) {
+                                JsonObject fields = ite.next().getAsJsonObject().get("fields").getAsJsonObject();
+                                Enfant enfant = new Enfant(fields.get("annee_naissance").getAsInt(), fields.get("prenom").getAsString(), fields.get("sexe").getAsString());
+                                dataEnfant.add(enfant);
+                            }
+                        }
+                    });
+            liste.setAdapter(dataEnfant);
         }
 }
