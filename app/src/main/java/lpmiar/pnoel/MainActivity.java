3b483073da40d31a45d98f802ca8528f8f062143
Jean Biancat <jean.biancat@etu.univ-nantes.fr>
2019-01-16 00:55:06 +0100

    Popup filtre

--- app/src/main/java/lpmiar/pnoel/MainActivity.java
+++ app/src/main/java/lpmiar/pnoel/MainActivity.java
@@ -1,11 +1,14 @@
 package lpmiar.pnoel;
 
 
+import android.app.Dialog;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -17,8 +20,6 @@ import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
-
-import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -29,37 +30,28 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Enfant> enfants = new ArrayList<>();
     private List<Enfant> enfantsFiltre = new ArrayList<>();
+    private List<String> tab = new ArrayList<>();
+    private Integer lastFiltreSexe = null;
+    private String lastFiltreDDN = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         setContentView(R.layout.activity_main);
-        final RadioGroup radioGroup = findViewById(R.id.radioGroup);
-        final Spinner ddn = findViewById(R.id.spinner);
-        List<String> tab = new ArrayList<>();
-        tab.add("ddn");
-        for (Integer i = 1990; i <2018;  i ++){
+
+        tab.add("Année de naissance");
+        for (Integer i = 2005; i <2018;  i ++){
             tab.add(i.toString());
         }
+
         super.onCreate(savedInstanceState);
-        Spinner listeAge = findViewById(R.id.spinner);
+
         final ListView liste = findViewById(R.id.liste);
         final EnfantArrayAdapter  dataEnfant = new EnfantArrayAdapter(this, R.layout.list_item_layout);
         liste.setAdapter(dataEnfant);
-        Button button = findViewById(R.id.filtrer);
-        button.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int selectedId = radioGroup.getCheckedRadioButtonId();
-                RadioButton rb = (RadioButton)findViewById(selectedId);
-                String date_de_naissance = ddn.getSelectedItem().toString();
 
-                filtre(rb.getText().toString(), date_de_naissance, liste, dataEnfant);
-            }
-        });
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tab);
-        listeAge.setAdapter(dataAdapter);
+
         String url = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_prenoms-enfants-nes-nantes&rows=1000&facet=prenom&facet=sexe&facet=annee_naissance";
 
         Ion.with(this)
@@ -81,6 +73,15 @@ public class MainActivity extends AppCompatActivity {
                             }
                     }
                 });
+
+        FloatingActionButton action = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+        action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callFiltreDialog(liste,dataEnfant);
+            }
+        });
     }
 
 
@@ -90,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
             if (sexe.contains("ç")) sexe = sexe.replace("ç", "c");
 
             if(sexe.equals("Tous")){
-                if(!annee_naissance.equals("ddn")){
+                if(!annee_naissance.equals("Année de naissance")){
                     //si tous les sexes et une date de naissance précise
                     for (Enfant e : enfants) {
                         if (String.valueOf(e.getAnneeNaissance()).equals(annee_naissance)) {
@@ -101,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
                     filtreCleared = clearFiltre(liste, enfantsAdapter);
                 }
             } else {
-                if(annee_naissance.equals("ddn")){
+                if(annee_naissance.equals("Année de naissance")){
                     //si un sexe précis et pas de date de naissance
                     for (Enfant e : enfants) {
                         if (e.getSexe().equals(sexe.toUpperCase())) {
@@ -132,4 +133,65 @@ public class MainActivity extends AppCompatActivity {
             liste.setAdapter(enfantsAdapter);
             return true;
         }
+
+    private void callFiltreDialog(final ListView liste, final EnfantArrayAdapter dataEnfant) {
+        final Dialog myDialog = new Dialog(this);
+        myDialog.setContentView(R.layout.popup_filtre);
+        myDialog.setCancelable(false);
+        Button filtrerBtn = (Button) myDialog.findViewById(R.id.filtrerBtn);
+        Button resetBtn = (Button) myDialog.findViewById(R.id.resetBtn);
+        ImageButton closeBtn = (ImageButton) myDialog.findViewById(R.id.imageButton);
+
+        final RadioGroup radioGroup = myDialog.findViewById(R.id.radioGroup);
+        final Spinner ddn = myDialog.findViewById(R.id.spinnerAge);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tab);
+        ddn.setAdapter(dataAdapter);
+
+        if(lastFiltreSexe != null){
+            radioGroup.check(lastFiltreSexe);
+        }
+
+        if (lastFiltreDDN != null && !lastFiltreDDN.isEmpty()){
+            Integer spinnerPosition = dataAdapter.getPosition(lastFiltreDDN);
+            ddn.setSelection(spinnerPosition);
+        }
+
+
+        myDialog.show();
+
+        filtrerBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) myDialog.findViewById(selectedId);
+                String date_de_naissance = ddn.getSelectedItem().toString();
+
+                lastFiltreSexe = selectedId;
+                lastFiltreDDN = date_de_naissance;
+
+                filtre(rb.getText().toString(), date_de_naissance, liste, dataEnfant);
+                myDialog.hide();
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearFiltre(liste, dataEnfant);
+                lastFiltreSexe = null;
+                lastFiltreDDN = null;
+                myDialog.hide();
+            }
+        });
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.hide();
+            }
+        });
+
+
+    }
 }
